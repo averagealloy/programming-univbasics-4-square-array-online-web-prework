aa9934e100291ff72abfcba57997e50cbe808d72
Mike McTighe <m.mctighe10@gmail.com>
2019-08-15 19:42:49 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(square_array)
 counter = 0
-while square_array[counter] < 3 do
-square_array[counter] ** 2
+while square_array[counter] do
+square_array = square_array[counter] ** 2
 counter += 1
 
 end
